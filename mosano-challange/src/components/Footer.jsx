fdf00cbbbd1ee94473af8e7bc5f1d9ba7a3f1925
Tiago Nunes <tiagonunes@MadMac.local>
2022-08-31 12:46:03 +0100

    Add save message

---
+++ mosano-challange/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer({ user }) {
+    return (
+    <div>{user.name} {user.surname}</div>
+)
+}
--- mosano-challange/src/components/List.jsx
+++ mosano-challange/src/components/List.jsx
@@ -1,19 +1,23 @@
 export default function List({ users }) {
+  console.log(users)
+  console.log(`users: ${Object.values(users)}`)
   return (
     <div>
       <table>
+        <tbody>
         <tr>
           <th>name</th>
           <th>country</th>
           <th>birthday</th>
         </tr>
         {users.map((user) => (
-          <tr>
+          <tr key={user.name}>
             <td key={user.name}>{user.name} {user.surname}</td>
             <td key={user.countries}>{user.countries}</td>
             <td key={user.birthday}>{user.birthday}</td>
           </tr>
         ))}
+        </tbody>
       </table>
     </div>
   );
